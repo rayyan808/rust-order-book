f9bb32d265d5f711f2e31d74f9ca49740616af9f
Rayyan <rayyan808@gmail.com>
2026-02-17 22:19:51 +0400

    create anchor project

---
+++ tests/src/test_initialize.rs
@@ -0,0 +1,28 @@
+use std::str::FromStr;
+
+use anchor_client::{
+    solana_sdk::{
+        commitment_config::CommitmentConfig, pubkey::Pubkey, signature::read_keypair_file,
+    },
+    Client, Cluster,
+};
+
+#[test]
+fn test_initialize() {
+    let program_id = "2QcfeiZAxohebjTdvv6S18Gxm77CEnjwWDr8P3sbU19u";
+    let anchor_wallet = std::env::var("ANCHOR_WALLET").unwrap();
+    let payer = read_keypair_file(&anchor_wallet).unwrap();
+
+    let client = Client::new_with_options(Cluster::Localnet, &payer, CommitmentConfig::confirmed());
+    let program_id = Pubkey::from_str(program_id).unwrap();
+    let program = client.program(program_id).unwrap();
+
+    let tx = program
+        .request()
+        .accounts(hello_world_2::accounts::Initialize {})
+        .args(hello_world_2::instruction::Initialize {})
+        .send()
+        .expect("");
+
+    println!("Your transaction signature {}", tx);
+}
